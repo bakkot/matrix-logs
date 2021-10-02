7295c3ab2074927effb7f8c33a7e1fbf8bff3ab3
Logbot <bakkot+matrix-logbot@gmail.com>
2021-10-02 16:20:09 -0700

    2021-10-02 16:20:09-07:00

---
+++ docs/render-roomlist.js
@@ -0,0 +1,44 @@
+
+"use strict";
+let rooms = [{"historical":false,"room":"Pattern Matching Champions"},{"historical":false,"room":"TC39 Beginners"},{"historical":false,"room":"TC39 Delegates"},{"historical":false,"room":"TC39 ECMA402"},{"historical":false,"room":"TC39 Editors"},{"historical":false,"room":"TC39 General"},{"historical":false,"room":"TC39 Implementers"},{"historical":false,"room":"TC39 Inclusion Group"},{"historical":false,"room":"TC39 Matrix Pilot Feedback"},{"historical":false,"room":"TC39 Structs and Shared Structs"},{"historical":false,"room":"TC39 Website"},{"historical":false,"room":"Temporal Dead Zone"},{"historical":false,"room":"WHATWG"},{"historical":false,"room":"testing-room-features"},{"historical":true,"room":"#tc39"},{"historical":true,"room":"#tc39-delegates"},{"historical":true,"room":"#tc39-editor-group"},{"historical":true,"room":"#whatwg"}];
+
+function sanitizeRoomName(room) {
+  if (room.startsWith('#')) {
+    room = 'irc-' + room;
+  }
+  return room.replace(/ /g, '_').replace(/#/g, '');
+}
+function renderRoom(room, current) {
+  return `<li><a href="${current === 'index' ? '' : '../'}${sanitizeRoomName(room)}/"${room === current ? ' class="current-room"' : ''}>${room}</a></li>`;
+}
+function renderRawRoomList(rooms, room) {
+  // someday, partition
+  let historicalRooms = rooms.filter(r => r.historical).map(r => r.room);
+  let activeRooms = rooms.filter(r => !r.historical).map(r => r.room);
+
+  if (historicalRooms.length === 0) {
+    return `
+<ul class="room-list">
+${activeRooms.map(r => renderRoom(r, room)).join('\n')}
+</ul>
+`;
+  } else {
+    return `
+<div class="room-list-wrapper">Active:<br>
+<ul class="room-list">
+${activeRooms.map(r => renderRoom(r, room)).join('\n')}
+</ul>
+</div>
+<br>
+<div class="room-list-wrapper">Historical:<br>
+<ul class="room-list">
+${historicalRooms.map(r => renderRoom(r, room)).join('\n')}
+</ul>
+</div>
+`;
+  }
+}
+
+addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.all-rooms').innerHTML = renderRawRoomList(rooms, room);
+});
